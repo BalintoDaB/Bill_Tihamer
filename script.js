62b46012e3da5fd248f74d1fe141f88a2dedafb6
sandorajsa <sandor.ajsa@students.jedlik.eu>
2023-10-23 13:26:28 +0200

    Bill Gates félig kész

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 function ChangeScene(sceneName) {
+    BasicsOn();
     if (sceneName == "Bill") {
+        const bill_adatok = ["lorem", "lorem", "lorem", "lorem", "lorem"]
         document.getElementById("nev").innerHTML = "Bill Gates";
-        document.getElementById("szul").innerHTML = "Octóber 28, 1955";
-        document.getElementById("vagyon").innerHTML = "97,4B dollár";
+        document.getElementById("szul").innerHTML = "1955. octóber 28.";
+        document.getElementById("elhunyt").innerHTML = "-";
         document.getElementById("ism").innerHTML = "Microsoft";
     }
     else if (sceneName == "Tihi") {
+        const tihi_adatok = ["lorem", "lorem", "lorem", "lorem", "lorem"]
         document.getElementById("nev").innerHTML = "Nemes Tihamér";
-        document.getElementById("szul").innerHTML = "Április 29, 1895";
-        document.getElementById("vagyon").innerHTML = "? forint";
+        document.getElementById("szul").innerHTML = "1895. április 29.";
+        document.getElementById("elhunyt").innerHTML = "1960. március 30.";
         document.getElementById("ism").innerHTML = "Kibernetikai gépek";
     }
+}
+
+function BasicsOn(){
+    document.getElementById("nev-t").innerHTML = "Név";
+    document.getElementById("szul-t").innerHTML = "Született";
+    document.getElementById("elhunyt-t").innerHTML = "Elhunyt";
+    document.getElementById("ism-t").innerHTML = "Fő ismerettség";
+    document.getElementById("adatok-t").innerHTML = "Fő adatok";
+    document.getElementById("adatok").innerHTML = "További információkért kattints a képre!";
+}
+
+function BasicScene(){
+    document.getElementById("nev").innerHTML = "";
+    document.getElementById("szul").innerHTML = "";
+    document.getElementById("elhunyt").innerHTML = "";
+    document.getElementById("ism").innerHTML = "";
+    document.getElementById("adatok").innerHTML = "";
+
+    document.getElementById("nev-t").innerHTML = "";
+    document.getElementById("szul-t").innerHTML = "";
+    document.getElementById("elhunyt-t").innerHTML = "";
+    document.getElementById("ism-t").innerHTML = "";
+    document.getElementById("adatok-t").innerHTML = "";
 }
